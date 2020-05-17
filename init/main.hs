23dc6ed49a2806b02ad6625cfd7b8f739e7c7758
haxul <s.s.starodubov@yandex.ru>
2020-05-17 19:29:06 +0400

    next step

--- init/main.hs
+++ init/main.hs
@@ -2,5 +2,12 @@ sumUs x y =  y + x
 doubleMe x = x + x
 doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x < 100 then x else doubleMe x 
+
 length' xs = sum [ 1 | _ <- xs]
+sumThree :: Int -> Int -> Int -> Int
+sumThree x y z = x + y + z
+
+sumThreeOne = sumThree 1
+
+removeUppercase :: [Char] -> [Char]
 removeUppercase xs = [x | x <- xs, x `elem` '\n' : ['a' .. 'z']]
