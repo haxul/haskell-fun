5765660e82fed38f8e3dbbbc09f1cf3f1d06f0e2
haxul <s.s.starodubov@yandex.ru>
2020-05-17 18:41:26 +0400

    removeUppercase

--- init/main.hs
+++ init/main.hs
@@ -2,3 +2,5 @@ sumUs x y =  y + x
 doubleMe x = x + x
 doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x < 100 then x else doubleMe x 
+length' xs = sum [ 1 | _ <- xs]
+removeUppercase xs = [x | x <- xs, x `elem` '\n' : ['a' .. 'z']]
